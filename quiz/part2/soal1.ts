45013ef9a8490d7da954f4e12e39281061a9db0c
imammunif <imammunif17@gmail.com>
2026-09-06 21:22:16 +0700

    phase 0 part 2 soal 1

---
+++ quiz/part2/soal1.ts
@@ -0,0 +1,17 @@
+// 1. Let's Form a Sentence
+// Problem:
+// Pada tugas ini kamu diminta untuk melakukan penambahan string menggunakan simbol +.
+// Disediakan variable word. Tambahkan nilai word satu per satu dengan nilai variable lain
+// untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
+// tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
+
+let word: string = 'JavaScript';
+let second: string = 'is';
+let third: string = 'awesome';
+let fourth: string = 'and';
+let fifth: string = 'I';
+let sixth: string = 'love';
+let seventh: string = 'it!';
+
+// Menghubungkan kata-kata menggunakan operator + dengan spasi pemisah tanpa membuat variabel baru
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
